848cff35ae1b6f74f5422aafc48b5215bb9fa3f8
Starovoytov Alexandr <43303621+stewkk@users.noreply.github.com>
2023-11-05 14:02:36 +0300

    Generates string test

--- 2/main_test.cpp
+++ 2/main_test.cpp
@@ -1,9 +1,17 @@
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+class RegexGenerator {
+  public:
+    RegexGenerator() {}
+
+    std::string GetString() {return "";}
+};
+
+TEST(GeneratorTest, GeneratesString) {
+  RegexGenerator g;
+
+  auto regex = g.GetString();
+
+  ASSERT_THAT(regex, testing::Eq(""));
 }
+
