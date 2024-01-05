aa2134557f67e6dd2bf43ac041a44e67f3deb6c5
Starovoytov Alexandr <43303621+stewkk@users.noreply.github.com>
2024-01-05 22:32:23 +0300

    Impl GetRandomStatesSequence

--- 2/regex_generator.cpp
+++ 2/regex_generator.cpp
@@ -1,40 +1,38 @@
+#include <sys/time.h>
+
 #include <cassert>
 #include <iostream>
 #include <random>
 #include <sstream>
 #include <string>
 
-#include <sys/time.h>
-
 class RegexGenerator {
-    public:
-        RegexGenerator(std::int32_t alphabet_power, std::int32_t star_height, std::int32_t max_letters);
-        std::string GenerateString();
+public:
+  RegexGenerator(std::int32_t alphabet_power, std::int32_t star_height, std::int32_t max_letters);
+  std::string GenerateString();
 
-    private:
-        std::int32_t alphabet_power_;
-        std::int32_t star_height_;
-        std::int32_t max_letters_;
+private:
+  std::int32_t alphabet_power_;
+  std::int32_t star_height_;
+  std::int32_t max_letters_;
 
-        std::int32_t current_star_height_;
+  std::int32_t current_star_height_;
 
-
-        void Regex(std::ostringstream& stream, std::int32_t letters_left);
-        void Binary(std::ostringstream& stream);
-        void Unary(std::ostringstream& stream);
-        void Symbol(std::ostringstream& stream);
+  void Regex(std::ostringstream& stream, std::int32_t letters_left);
+  void Binary(std::ostringstream& stream);
+  void Unary(std::ostringstream& stream);
+  void Symbol(std::ostringstream& stream);
 };
 
 std::int32_t Random(std::vector<double> probs) {
-  timeval t1;
-  gettimeofday(&t1, NULL);
-  static std::mt19937 generator(t1.tv_usec * t1.tv_sec);
+  std::random_device rd;
+  static std::mt19937 generator(rd());
   std::uniform_int_distribution<std::int32_t> distribution(0, 10000);
-  const double rnd = distribution(generator)/10000.0;
+  const double rnd = distribution(generator) / 10000.0;
   double lhs = 0;
   for (std::int32_t v = 0; v < probs.size(); ++v) {
     const auto& prob = probs[v];
-    if (rnd >= lhs && rnd <= lhs+prob) {
+    if (rnd >= lhs && rnd <= lhs + prob) {
       return v;
     }
     lhs += prob;
@@ -45,17 +43,18 @@ std::int32_t Random(std::vector<double> probs) {
 void RegexGenerator::Regex(std::ostringstream& stream, std::int32_t letters_left) {
   assert(letters_left > 0);
   if (letters_left == 1) {
-      Symbol(stream);
-      if (const auto rnd_value = Random({0.5, 0.5}); current_star_height_+1 <= star_height_ && rnd_value == 0) {
-          current_star_height_++;
-          Unary(stream);
-      }
-      return;
+    Symbol(stream);
+    if (const auto rnd_value = Random({0.5, 0.5});
+        current_star_height_ + 1 <= star_height_ && rnd_value == 0) {
+      current_star_height_++;
+      Unary(stream);
+    }
+    return;
   }
   const auto rnd_value = Random({0.25, 0.1, 0.35, 0.3});
   switch (rnd_value) {
     case 0:
-      Regex(stream, letters_left-1);
+      Regex(stream, letters_left - 1);
       Binary(stream);
       Regex(stream, 1);
       break;
@@ -65,13 +64,13 @@ void RegexGenerator::Regex(std::ostringstream& stream, std::int32_t letters_left
       stream << ')';
       break;
     case 2:
-        if (current_star_height_+1 <= star_height_) {
-            current_star_height_++;
-            Regex(stream, letters_left);
-            Unary(stream);
-        } else {
-            Symbol(stream);
-        }
+      if (current_star_height_ + 1 <= star_height_) {
+        current_star_height_++;
+        Regex(stream, letters_left);
+        Unary(stream);
+      } else {
+        Symbol(stream);
+      }
       break;
     case 3:
       Symbol(stream);
@@ -91,32 +90,30 @@ void RegexGenerator::Binary(std::ostringstream& stream) {
   }
 }
 
-void RegexGenerator::Unary(std::ostringstream& stream) {
-  stream << '*';
-}
+void RegexGenerator::Unary(std::ostringstream& stream) { stream << '*'; }
 
 void RegexGenerator::Symbol(std::ostringstream& stream) {
   const double p = 1.0 / alphabet_power_;
   std::vector<double> probs;
   for (std::int32_t i = 0; i < alphabet_power_; i++) {
-      probs.push_back(p);
+    probs.push_back(p);
   }
   stream << (std::vector{'a', 'b', 'c', 'd', 'e'})[Random(probs)];
 }
 
 std::string RegexGenerator::GenerateString() {
-    current_star_height_ = 0;
-    std::ostringstream res;
-    Regex(res, max_letters_);
-    return res.str();
+  current_star_height_ = 0;
+  std::ostringstream res;
+  Regex(res, max_letters_);
+  return res.str();
 }
 
-RegexGenerator::RegexGenerator(std::int32_t alphabet_power, std::int32_t star_height, std::int32_t max_letters) : alphabet_power_(alphabet_power),
-                                                                                                    star_height_(star_height), max_letters_(max_letters)
-{}
+RegexGenerator::RegexGenerator(std::int32_t alphabet_power, std::int32_t star_height,
+                               std::int32_t max_letters)
+    : alphabet_power_(alphabet_power), star_height_(star_height), max_letters_(max_letters) {}
 
 std::int32_t main() {
-    RegexGenerator generator(2, 2, 1000);
-    std::cout << generator.GenerateString() << std::endl;
-    return 0;
+  RegexGenerator generator(2, 2, 1000);
+  std::cout << generator.GenerateString() << std::endl;
+  return 0;
 }
--- 2/string_generator_test.cpp
+++ 2/string_generator_test.cpp
@@ -1,65 +1,114 @@
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 #include <iostream>
+#include <random>
 #include <sstream>
-#include <vector>
 #include <string>
+#include <unordered_set>
+#include <vector>
 
 using std::istringstream;
+using testing::Contains;
 using testing::Eq;
 
-struct Automata {
-    std::vector<std::string> states;
-    std::vector<std::size_t> accepting_states;
-    std::vector<std::vector<std::string>> transitions;
+struct Transition {
+  std::string symbols;
+  std::size_t state;
 
-    bool operator ==(const Automata& other) const = default;
+  bool operator==(const Transition &other) const = default;
 };
 
-Automata ReadAutomata(std::istream& in) {
-    std::size_t n;
-    in >> n;
-    Automata res;
-    res.states.assign(n, {});
-    res.transitions.assign(n, std::vector<std::string>(n));
-    for (auto& el : res.states) {
-        in >> el;
+struct Automata {
+  std::vector<std::string> states;
+  std::unordered_set<std::size_t> accepting_states;
+  std::vector<std::vector<Transition>> transitions;
+
+  bool operator==(const Automata &other) const = default;
+};
+
+Automata ReadAutomata(std::istream &in) {
+  std::size_t n;
+  in >> n;
+  Automata res;
+  res.states.assign(n, {});
+  for (auto &el : res.states) {
+    in >> el;
+  }
+  std::size_t accepting_states_count;
+  in >> accepting_states_count;
+  for (std::size_t i = 0; i < accepting_states_count; i++) {
+    std::size_t state;
+    in >> state;
+    res.accepting_states.insert(state);
+  }
+  res.transitions.assign(n, {});
+  for (auto &row : res.transitions) {
+    for (std::size_t i = 0; i < n; i++) {
+      std::string transition;
+      in >> transition;
+      if (transition != "0") {
+        row.push_back({transition, i});
+      }
     }
-    std::size_t accepting_states_count;
-    in >> accepting_states_count;
-    res.accepting_states.assign(accepting_states_count, {});
-    for (auto& el : res.accepting_states) {
-        in >> el;
-    }
-    for (auto& row : res.transitions) {
-        for (auto& el : row) {
-            in >> el;
-        }
-    }
-    return res;
+  }
+  return res;
 }
 
-using ReachabilityMatrix = std::vector<std::vector<bool>>;
+using ReachabilityMatrix = std::vector<std::vector<std::size_t>>;
 
-ReachabilityMatrix GetReachabilityMatrix(const Automata& a) {
-    const auto n = a.states.size();
-    ReachabilityMatrix res(n, ReachabilityMatrix::value_type(n));
+ReachabilityMatrix GetReachabilityMatrix(const Automata &a) {
+  const auto n = a.states.size();
+  std::vector<std::vector<bool>> dp(n, std::vector<bool>(n));
+  for (std::size_t i = 0; i < n; i++) {
+    for (auto &el : a.transitions[i]) {
+      dp[i][el.state] = true;
+    }
+  }
+  for (std::size_t k = 0; k < n; k++) {
     for (std::size_t i = 0; i < n; i++) {
-        for (std::size_t j = 0; j < n; j++) {
-            if (!a.transitions[i][j].empty()) {
-                res[i][j] = true;
-            }
-        }
+      for (std::size_t j = 0; j < n; j++) {
+        dp[i][j] = dp[i][j] || (dp[i][k] && dp[k][j]);
+      }
     }
-    for (std::size_t k = 0; k < n; k++) {
-        for (std::size_t i = 0; i < n; i++) {
-            for (std::size_t j = 0; j < n; j++) {
-                res[i][j] = res[i][j] || (res[i][k] && res[k][j]);
-            }
-        }
+  }
+  ReachabilityMatrix res(n);
+  for (std::size_t i = 0; i < n; i++) {
+    for (std::size_t j = 0; j < n; j++) {
+      if (dp[i][j]) {
+        res[i].push_back(j);
+      }
     }
-    return res;
+  }
+  return res;
+}
+
+std::vector<std::size_t> GetRandomStateSequence(const ReachabilityMatrix &reachability,
+                                                 const Automata &automata) {
+  std::vector<std::size_t> res = {0};
+
+  std::random_device rd;
+  std::mt19937 generator(rd());
+
+  auto min_states_count = std::uniform_int_distribution<std::int32_t>(0, 20)(generator);
+
+  for (std::size_t i = 0; i < static_cast<std::size_t>(min_states_count); i++) {
+    const auto& current_state_reachability = reachability[res.back()];
+    auto next = std::uniform_int_distribution<std::int32_t>(0, current_state_reachability.size()-1)(generator);
+    res.push_back(next);
+  }
+  if (!automata.accepting_states.contains(res.back())) {
+    for (auto next : reachability[res.back()]) {
+      // NOTE: choose random final state here?
+      if (automata.accepting_states.contains(next)) {
+        res.push_back(next);
+      }
+    }
+  }
+  if (!automata.accepting_states.contains(res.back())) {
+    throw std::logic_error{"Can't build random state sequence"};
+  }
+  return res;
 }
 
 TEST(StringGeneratorTest, ReadsAutomata) {
@@ -69,22 +118,37 @@ TEST(StringGeneratorTest, ReadsAutomata) {
 
   ASSERT_THAT(got, Eq(Automata{{"(bb)*", "b(bb)*"},
                                {0},
-                                    {
-                                        {"0", "ab"},
-                                        {"b", "0"},
-                                    }}));
+                               {
+                                   {{"ab", 1}},
+                                   {{"b", 0}},
+                               }}));
 }
 
 TEST(StringGeneratorTest, BuildsReachabilityMatrix) {
-    Automata a{{"(bb)*", "b(bb)*"},
-               {0},
-               {
-                   {"0", "ab"},
-                   {"b", "0"},
-               }};
+  Automata a{{"(bb)*", "b(bb)*"},
+             {0},
+             {
+                 {{"ab", 1}},
+                 {{"b", 0}},
+             }};
 
-    ReachabilityMatrix got = GetReachabilityMatrix(a);
+  ReachabilityMatrix got = GetReachabilityMatrix(a);
 
-    ASSERT_THAT(got, Eq(ReachabilityMatrix{{1, 1}, {1, 1}}));
+  ASSERT_THAT(got, Eq(ReachabilityMatrix{{0, 1}, {0, 1}}));
+}
+
+TEST(StringGeneratorTest, BuildsRandomStateSequence) {
+  Automata a{{"(bb)*", "b(bb)*"},
+             {0},
+             {
+                 {{"ab", 1}},
+                 {{"b", 0}},
+             }};
+  ReachabilityMatrix reachability = GetReachabilityMatrix(a);
+
+  auto got = GetRandomStateSequence(reachability, a);
+
+  EXPECT_THAT(got.front(), Eq(0));
+  EXPECT_THAT(a.accepting_states, Contains(got.back()));
 }
 
