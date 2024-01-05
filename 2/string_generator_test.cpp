e3d197858c596993f92bd83fca7b76a8eca5d9e3
Starovoytov Alexandr <43303621+stewkk@users.noreply.github.com>
2024-01-05 20:55:10 +0300

    Add reachability matrix and accepting states

--- 2/string_generator_test.cpp
+++ 2/string_generator_test.cpp
@@ -11,6 +11,7 @@ using testing::Eq;
 
 struct Automata {
     std::vector<std::string> states;
+    std::vector<std::size_t> accepting_states;
     std::vector<std::vector<std::string>> transitions;
 
     bool operator ==(const Automata& other) const = default;
@@ -25,6 +26,12 @@ Automata ReadAutomata(std::istream& in) {
     for (auto& el : res.states) {
         in >> el;
     }
+    std::size_t accepting_states_count;
+    in >> accepting_states_count;
+    res.accepting_states.assign(accepting_states_count, {});
+    for (auto& el : res.accepting_states) {
+        in >> el;
+    }
     for (auto& row : res.transitions) {
         for (auto& el : row) {
             in >> el;
@@ -33,12 +40,35 @@ Automata ReadAutomata(std::istream& in) {
     return res;
 }
 
+using ReachabilityMatrix = std::vector<std::vector<bool>>;
+
+ReachabilityMatrix GetReachabilityMatrix(const Automata& a) {
+    const auto n = a.states.size();
+    ReachabilityMatrix res(n, ReachabilityMatrix::value_type(n));
+    for (std::size_t i = 0; i < n; i++) {
+        for (std::size_t j = 0; j < n; j++) {
+            if (!a.transitions[i][j].empty()) {
+                res[i][j] = true;
+            }
+        }
+    }
+    for (std::size_t k = 0; k < n; k++) {
+        for (std::size_t i = 0; i < n; i++) {
+            for (std::size_t j = 0; j < n; j++) {
+                res[i][j] = res[i][j] || (res[i][k] && res[k][j]);
+            }
+        }
+    }
+    return res;
+}
+
 TEST(StringGeneratorTest, ReadsAutomata) {
-  istringstream input{"2\n(bb)* b(bb)*\n0 ab\nb 0\n"};
+  istringstream input{"2\n(bb)* b(bb)*\n1 0\n0 ab\nb 0\n"};
 
-  auto automata = ReadAutomata(input);
+  auto got = ReadAutomata(input);
 
-  ASSERT_THAT(automata, Eq(Automata{{"(bb)*", "b(bb)*"},
+  ASSERT_THAT(got, Eq(Automata{{"(bb)*", "b(bb)*"},
+                               {0},
                                     {
                                         {"0", "ab"},
                                         {"b", "0"},
@@ -46,5 +76,15 @@ TEST(StringGeneratorTest, ReadsAutomata) {
 }
 
 TEST(StringGeneratorTest, BuildsReachabilityMatrix) {
+    Automata a{{"(bb)*", "b(bb)*"},
+               {0},
+               {
+                   {"0", "ab"},
+                   {"b", "0"},
+               }};
 
+    ReachabilityMatrix got = GetReachabilityMatrix(a);
+
+    ASSERT_THAT(got, Eq(ReachabilityMatrix{{1, 1}, {1, 1}}));
 }
+
